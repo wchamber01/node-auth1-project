648b7a68561274a432eb60154d9ab49c4e5735de
wchamber01 <williamchambers@gmail.com>
2020-01-28 11:30:37 -0500

    got users path working with validation

--- restricted/rest-router.js
+++ restricted/rest-router.js
@@ -1,19 +1,24 @@
 const bc = require("bcryptjs");
-const router = require("express").Router();
 
 const Users = require("../users/users-model.js");
 
 function restricted(req, res, next) {
-  if (req.headers.authorization) {
-    bc.hash(req.headers.authorization, 8, (err, hash) => {
-      if (err) {
-        res.status(500).json({ oops: "it broke" });
-      } else {
-        res.status(200).json(next());
-      }
-    });
-  } else {
-    res.status(401).json({ error: "You shall not pass!" });
+  const { username, password } = req.headers;
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        console.log(user, "user line 39");
+        if (user && bc.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: "YOU SHALL NOT PASS!!!" });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({ error: "There was a problem" });
+      });
   }
 }
-module.exports = router;
+module.exports = restricted;
